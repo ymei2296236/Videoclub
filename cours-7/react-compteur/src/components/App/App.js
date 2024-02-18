338906ed4af53139fb8a4447d10bd47f47061b99
ymei2296236 <e2296236@cmaisonneuve.qc.ca>
2024-02-18 18:50:55 -0500

    Ajouter commentaires au main

--- cours-7/react-compteur/src/components/App/App.js
+++ cours-7/react-compteur/src/components/App/App.js
@@ -6,18 +6,23 @@ import './App.css';
 
 function App() {
 
+
   const aValeursInitiales = [0, 0, 2, 0];
+
+  // Calcule le total initial
   const [total, setTotal] = useState(aValeursInitiales.reduce((a, b) => a + b, 0))
 
-  function totalCompteur(valeurCompteur)
+  // Calcule le nouveau total 
+  function handleTotal(valeurCompteur)
   {
     setTotal((totalActuel) => totalActuel + valeurCompteur);
+
   }
 
-  
+  // Crée et insère les compteurs avec les valeurs initiales et le total
   const compteurs = aValeursInitiales.map((valeurInitiale, index) =>
   {
-    return <Compteur key={index} valeurInitiale={valeurInitiale} handleTotal={totalCompteur} />
+    return <Compteur key={index} valeurInitiale={valeurInitiale} handleTotal={handleTotal} />
   })
  
   return (
@@ -29,6 +34,7 @@ function App() {
     </main>
     </>
   );
+
 
 }
 
--- cours-7/react-compteur/src/components/Total/Total.js
+++ cours-7/react-compteur/src/components/Total/Total.js
@@ -1,15 +1,6 @@
-import { useState } from 'react';
 import './Total.css';
 
 function Total(props) {
-
-    // const [total, setTotal] = useState(aValeursInitiales.reduce((a, b) => a + b, 0))
-
-    // function totalCompteur(valeurCompteur)
-    // {
-    //   setTotal((totalActuel) => totalActuel + valeurCompteur);
-    // }
-    
  
   return (
       <div className='total'>
